7fce6e5e36809dbd2f27bb2986894f8e24758c2a
Jon Hadfield <jon@lessknown.co.uk>
2012-08-13 21:43:53 +0100

    backup of some half working bits

--- main.c
+++ main.c
@@ -15,12 +15,13 @@ strcpy(request1_ptr->client_ip,"8.8.8.8");
 strcpy(request1_ptr->date,"28/03/1977");
 
 printf("request1 has ip of: %s\n",request1_ptr->client_ip);
-printf("request1 has date of: %s",request1_ptr->date);
+printf("request1 has date of: %s\n",request1_ptr->date);
 
 
   FILE *pRead;
   /*req reqs[10]; */
-  int count;
+  /*int count;*/
+  /*char log_line[500];*/
   pRead = fopen("log.log", "r");
   if ( pRead == NULL )  
   {
@@ -28,7 +29,21 @@ printf("request1 has date of: %s",request1_ptr->date);
     exit(1);
   }
 
+/*
   fscanf(pRead, "%d", &count);
+  fscanf(pRead, "%[^\n]", &log_line);
+   
+  printf("line: %s\n", log_line);
+*/
+  int count1;
+  char aline[500];
+  /*char req_ip[15];*/
+  while (fgets(aline, 500, pRead) != NULL) {
+    /*sscanf(aline, "%s %s", array[count1][0],array[count1][1]);*/
+    printf("line %d: %s\n",count1,aline);
+    count1++;
+  }
+  printf("File contains: %d lines\n",count1); 
 
 /*for(k = 0; k < count; k++)
 {
