51bc80262859e83f4615e12c91456dbb4271c3d6
Jon Hadfield <jon@lessknown.co.uk>
2012-08-17 23:59:21 +0100

    load log into array of struct and then output

--- main.c
+++ main.c
@@ -1,76 +1,36 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include <string.h>
 
-struct st_http_request {
-	char ip[15];
-	char ident[10];
-};
+typedef struct {
+	char ip[150];
+	char ident[100];
+} st_http_request;
 
 int main (int argc, char *argv[]) {
 
   FILE *pRead;
-  char log_line[5000];
   pRead = fopen("log.log", "r");
 
-//Create array of 20 of st_http_request
-  struct st_http_request **temp_struct;
-  temp_struct = malloc(20 * sizeof *temp_struct);
+  st_http_request *p,*tmp;
+  p = (st_http_request *)malloc(sizeof(st_http_request)*10);
+  printf("initial size of temp_struct: %d\n",sizeof(st_http_request));
 
-  if (temp_struct == NULL) { printf("Pants"); }
-
-  //int i=0;
-  //for (i = 0; i < 20; ++i) {
-  //  temp_struct[i] = malloc(sizeof *temp_struct[i]);
-  //  strcpy(temp_struct[i]->ip,"1.2.3.4");
-  //  printf("log_line %d: ip:%s\n",i,temp_struct[i]->ip);
-  //}
-
+  char log_line[600];
   int counter = 0;
-  while (fgets(log_line, 500, pRead) != NULL) {
-    temp_struct[counter] = malloc(sizeof *temp_struct[counter]);
-
-    //struct st_temp **tmp;
-    //tmp = realloc(temp_struct, 30 * sizeof *temp_struct);
-    //if (tmp == NULL) { 
-	// handle failed realloc, temp_struct is unchanged
-    //} else {
-	// everything went ok, update the original pointer (temp_struct)
-	//temp_struct = tmp; 
-    //}
-
-    sscanf(log_line, "%s %s", temp_struct[counter]->ip, temp_struct[counter]->ident);
-    printf("log_line %d: ip:%s ident:%s\n",counter,temp_struct[counter]->ip,temp_struct[counter]->ident);
+  while (fgets(log_line, 600, pRead) != NULL) {
+    //Sanity check log line
+    sscanf(log_line, "%s %s", p[counter].ip, p[counter].ident);
+    printf("log_line %d: ip:%s ident:%s\n",counter,p[counter].ip,p[counter].ident);
     counter++;
+    tmp = (st_http_request *) realloc ( p, (counter+4) * sizeof(st_http_request) );
+    if (tmp == NULL) { 
+      printf("pants!!!");
+    } else {
+      p = tmp; 
+    }
   }
 
-
-  // We need more space ... remember to use a temporary variable
-  //struct st_http_request **tmp;
-  //tmp = realloc(temp_struct, 30 * sizeof *temp_struct);
-  //if (tmp == NULL) { 
-  //  printf("Cannot allocate any more memory. Pants :(");
-    // handle failed realloc, temp_struct is unchanged
-  //} else {
-    // everything went ok, update the original pointer (temp_struct)
-  //  temp_struct = tmp; 
-  //}
-  //int j=0;
-  //for (i = 20; i < 30; ++i) { // notice the indexing, [20..30)
-    // NOTICE: the realloc allocated more space for pointers
-    // we still need to allocate space for each new object
-  //  temp_struct[i] = malloc(sizeof *temp_struct[i]);
-  //  strcpy(temp_struct[i]->ip,"8.8.8.8");
-  //}
-  // temp_struct now "holds" 30 temp_struct objects
-  // ...
-  // and always do remember, in the end
-  //int k=0;
-  //for (k = 0; i < 30; ++i)
-  //  free(temp_struct[i]);
-  //  free(temp_struct);
-
+  free(p);
   return(0);
 }
