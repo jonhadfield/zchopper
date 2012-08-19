7a62025a25b217520803fab009934ecb1c1a2c8e
Jon Hadfield <hadfieldj@gmail.com>
2012-08-19 13:48:36 +0100

    Add debugging of line properties, modify request struct to introduce vars to store combined format entries

--- main.c
+++ main.c
@@ -2,10 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define  READ_LINE_LENGTH   600
+#define  MAX_LINE_LENGTH   500
+
 typedef struct {
-	char ip[150];
-	char ident[100];
+	char req_ip[15];
+	char req_ident[100];
+	char req_user[100];
+	char req_date[100];
+	char req_time[100];
+	char req_line[100];
+	char resp_code[100];
+	char resp_size[100];
+	char req_referer[100];
+	char req_user_agent[100];
 } st_http_request;
+
 
 int main (int argc, char *argv[]) {
 
@@ -14,14 +26,32 @@ int main (int argc, char *argv[]) {
 
   st_http_request *p,*tmp;
   p = (st_http_request *)malloc(sizeof(st_http_request)*10);
-  printf("initial size of temp_struct: %d\n",sizeof(st_http_request));
 
-  char log_line[600];
+  char log_line[READ_LINE_LENGTH];
   int counter = 0;
-  while (fgets(log_line, 600, pRead) != NULL) {
-    //Sanity check log line
-    sscanf(log_line, "%s %s", p[counter].ip, p[counter].ident);
-    printf("log_line %d: ip:%s ident:%s\n",counter,p[counter].ip,p[counter].ident);
+  int line_length = 0;
+  while (fgets(log_line, 1024, pRead) != NULL) {
+
+    line_length = strlen(log_line);
+    //DEBUG
+    printf("line no.: %d\n",counter);
+    printf("logline: %s\n",log_line);
+    printf("logline length: %d\n",line_length);
+    printf("penult char is: %c\n",log_line[line_length-3]);
+    printf("last char is: %c\n\n",log_line[line_length-2]);
+
+    if ( line_length > MAX_LINE_LENGTH ) {
+      printf("Line: %d exceeds max request length of: %d\n", counter, MAX_LINE_LENGTH);
+      continue;
+    }
+
+    sscanf(log_line, "%s %s %s %s %s %s %s %s %s %s", \
+                     p[counter].req_ip, p[counter].req_ident, \
+                     p[counter].req_user, p[counter].req_date, \
+                     p[counter].req_time, p[counter].req_line, \
+                     p[counter].resp_code, p[counter].resp_size, \
+                     p[counter].req_referer,p[counter].req_user_agent);
+    printf("log_line %d: ip:%s ident:%s user:%s date:%s\n",counter,p[counter].req_ip,p[counter].req_ident,p[counter].req_user,p[counter].req_date);
     counter++;
     tmp = (st_http_request *) realloc ( p, (counter+4) * sizeof(st_http_request) );
     if (tmp == NULL) { 
