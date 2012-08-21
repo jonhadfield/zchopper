b4b02e9376440a1b701dedf390e11f413f2716c7
Jon Hadfield <hadfieldj@gmail.com>
2012-08-22 00:11:05 +0100

    Take short and long options using getopt

--- src/main.c
+++ src/main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
 #include <string.h>
+
+/* short options:
+ * 
+ * -o outfile name - write to file rather than stdout
+ * -t output type 
+ * -b batch size - number of lines to process before flushing - debug only
+ * -i ip of db server 
+ * -p port of db server 
+ * -v verbose output
+ * additional file names are used as input files
+ * 
+ *
+ * long options:
+ *
+ * --outfile
+ * --type
+ * --batchsize
+ * --ip
+ * --port
+ * --verbose
+ *
+ */
 
 #define  MAX_LINE_LENGTH  8192 
 #define  MAX_IP             15
@@ -30,22 +54,57 @@ typedef struct {
 	char req_agent[MAX_AGENT];
 } st_http_request;
 
-int main (int argc, char *argv[]) {
-int running_total = 0;
-  if(argc < 2) {
-    printf( "usage: %s filename\n", argv[0] );
-    exit(1);
-  } else { 
+struct globalArgs_t {
+	const char *outFileName;	        /* -o option */
+	FILE *outFile;
+	char *type;				/* -t option */
+	int  batch_size;			/* -b option */
+	char *ip;				/* -i option */
+	char *port;				/* -p option */
+	int verbose;				/* verbosity */
+	char **inputFiles;			/* input files */
+	int numInputFiles;			/* # of input files */
+} globalArgs;
+
+static const char *optString = "o:t:b:i:p:vh?";
+
+static const struct option longOpts[] = {
+	{ "outFileName", no_argument, NULL, 'o' },
+	{ "type", required_argument, NULL, 't' },
+	{ "batch_size", no_argument, NULL, 'b' },
+	{ "ip", no_argument, NULL, 'i' },
+	{ "port", no_argument, NULL, 'p' },
+	{ "verbose", no_argument, NULL, 'v' },
+	{ "help", no_argument, NULL, 'h' },
+	{ NULL, no_argument, NULL, 0 }
+};
+
+void display_usage( void )
+{
+	puts( "chopper - reads apache logs and outputs to file or db" );
+	exit( EXIT_FAILURE );
+}
+
+/* Convert the input files to HTML, governed by globalArgs.
+ *  */
+int chop( void )
+{
+	printf( "outFileName: %s\n", globalArgs.outFileName );
+	printf( "type: %s\n", globalArgs.type );
+	printf( "batch_size: %d\n", globalArgs.batch_size );
+	printf( "ip: %s\n", globalArgs.ip);
+	printf( "port: %s\n", globalArgs.port);
+	printf( "verbose: %d\n", globalArgs.verbose );
+	printf( "numInputFiles: %d\n", globalArgs.numInputFiles );
+	int running_total = 0;
    //LOOP THROUGH FILES
     FILE *pRead;
     int f_count;  
-    for (f_count = 1; f_count < argc; f_count++) {
-      printf("File %d: %s\n",f_count,argv[f_count]);
-      pRead = fopen(argv[f_count], "r");
-
+    for (f_count = 0; f_count < globalArgs.numInputFiles; f_count++) {
+      printf("File %d: %s\n",f_count,globalArgs.inputFiles[f_count]);
+      pRead = fopen(globalArgs.inputFiles[f_count], "r");
       st_http_request *p,*tmp;
       p = (st_http_request *)malloc(sizeof(st_http_request)*2);
-
       char log_line[MAX_LINE_LENGTH];
       int counter = 0;
       int line_length = 0;
@@ -63,7 +122,6 @@ int running_total = 0;
 	    p[counter].req_proto, \
 	    p[counter].resp_code, p[counter].resp_bytes, \
 	    p[counter].req_referer, p[counter].req_agent);
-
 	  tmp = (st_http_request *) realloc ( p, (counter+2) * sizeof(st_http_request) );
 	  if (tmp == NULL) { 
 	    printf("Failed to increase memory allocation.");
@@ -85,7 +143,58 @@ int running_total = 0;
     printf("Scanned: %d lines.\n",running_total);
     fclose(pRead);
     free(p);
-    }
   }
-    return(0);
-  }
+  return(0);
+}
+int main (int argc, char *argv[]) {
+	int opt = 0;
+	int longIndex = 0;
+	globalArgs.outFileName = NULL;
+	globalArgs.outFile = NULL;
+	globalArgs.type = NULL;
+	globalArgs.batch_size = BATCH_SIZE;
+	globalArgs.ip = NULL;
+	globalArgs.port = NULL;
+	globalArgs.verbose = 0;
+	globalArgs.inputFiles = NULL;
+	globalArgs.numInputFiles = 0;
+	
+	opt = getopt_long( argc, argv, optString, longOpts, &longIndex );
+	while( opt != -1 ) {
+		switch( opt ) {
+			case 'o':
+				globalArgs.outFileName = optarg;
+				break;
+			case 't':
+				globalArgs.type = optarg;
+				break;
+			case 'b':
+				globalArgs.batch_size = atoi(optarg);
+				break;
+			case 'i':
+				globalArgs.ip = optarg;
+				break;
+			case 'p':
+				globalArgs.port = optarg;
+				break;
+			case 'v':
+				globalArgs.verbose = 1;
+				break;
+			case 'h':
+			case '?':
+				display_usage();
+				break;
+			case 0:	
+				break;
+			default:
+				break;
+		}
+		
+		opt = getopt_long( argc, argv, optString, longOpts, &longIndex );
+	}
+	
+	globalArgs.inputFiles = argv + optind;
+	globalArgs.numInputFiles = argc - optind;
+	chop();
+        return(0);
+}
