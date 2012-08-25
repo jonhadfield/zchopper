f7f369bb62b60bc8dbcca8592413bd4db9d818e2
Jon Hadfield <hadfieldj@gmail.com>
2012-08-25 12:41:23 +0100

    Renamed main.c to chopper.c and moved macros to header file

--- src/chopper.c
+++ src/chopper.c
@@ -3,43 +3,7 @@
 #include <unistd.h>
 #include <getopt.h>
 #include <string.h>
-
-/* short options:
- * 
- * -o outfile name - write to file rather than stdout
- * -t output type 
- * -b batch size - number of lines to process before flushing - debug only
- * -i ip of db server 
- * -p port of db server 
- * -s search string
- * -v verbose output
- * additional file names are used as input files
- * 
- *
- * long options:
- *
- * --outfile
- * --type
- * --batchsize
- * --ip
- * --port
- * --search_string
- * --verbose
- *
- */
-
-#define  MAX_LINE_LENGTH  8192
-#define  MAX_IP             15
-#define  MAX_IDENT           8
-#define  MAX_USER            8
-#define  MAX_DATETIME       22
-#define  MAX_METHOD          7
-#define  MAX_URI          2083	//Safe limit?
-#define  MAX_PROTO           8
-#define  MAX_RESP_BYTES     20
-#define  MAX_REFERER      2083	//Safe limit?
-#define  MAX_AGENT        1024	//Safe limit?
-#define  BATCH_SIZE       1000
+#include "chopper.h"
 
 const char chopper_usage_string[] =
 	"chopper [-s|--search_string] [-o|--outfile <path>] [-t|--type] [-b|--batchsize <value>] [-i|--ip <value>]\n"
---
+++ src/chopper.h
@@ -0,0 +1,36 @@
+#define  MAX_LINE_LENGTH  8192
+#define  MAX_IP             15
+#define  MAX_IDENT           8
+#define  MAX_USER            8
+#define  MAX_DATETIME       22
+#define  MAX_METHOD          7
+#define  MAX_URI          2083  //Safe limit?
+#define  MAX_PROTO           8
+#define  MAX_RESP_BYTES     20
+#define  MAX_REFERER      2083  //Safe limit?
+#define  MAX_AGENT        1024  //Safe limit?
+#define  BATCH_SIZE       1000
+
+/* short options:
+*
+* -o outfile name - write to file rather than stdout
+* -t output type
+* -b batch size - number of lines to process before flushing - debug only
+* -i ip of db server
+* -p port of db server
+* -s search string
+* -v verbose output
+* additional file names are used as input files
+*
+*
+* long options:
+*
+* --outfile
+* --type
+* --batchsize
+* --ip
+* --port
+* --search_string
+* --verbose
+*
+*/
